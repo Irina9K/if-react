4832f4b8b66ad5e605d888f921c4eddea0151eeb
Irina9K <irina121291992@gmail.com>
2022-08-11 01:19:03 +0300

    added dependens

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'babel', 'import'],
+  rules: {
+    'object-curly-newline': [
+      'error',
+      {
+        ObjectExpression: 'always',
+        ObjectPattern: { multiline: true },
+        ImportDeclaration: 'never',
+        ExportDeclaration: { multiline: true, minProperties: 3 },
+      },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 'off',
+    'max-len': ['error', { ignoreComments: true, code: 160 }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 0,
+    'operator-linebreak': [
+      'error',
+      'after',
+      {
+        overrides: {
+          ':': 'before',
+        },
+      },
+    ],
+    'max-classes-per-file': ['error', { ignoreExpressions: true, max: 5 }],
+  },
+};
