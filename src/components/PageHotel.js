303121aab0c784a2d7105c0739f7dc34bd6e3ed0
Irina9K <irina121291992@gmail.com>
2022-09-16 14:19:40 +0300

    commit for my mentor3

--- src/components/PageHotel.js
+++ src/components/PageHotel.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const PageHotel = () => (
-    <div>
-      <div>Страница перехода</div>
-    </div>
+  <div>
+    <div>Страница перехода</div>
+  </div>
 );
 
 export default PageHotel;
